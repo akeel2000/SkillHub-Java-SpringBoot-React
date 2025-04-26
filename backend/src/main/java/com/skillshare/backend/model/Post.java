35202d2819040b234740f8e08e5d29eb8be5c61f
rahufath <rahufath.shajeeh52@gmail.com>
2025-04-26 13:40:17 +0530

    add some file from shajeeh

--- backend/src/main/java/com/skillshare/backend/model/Post.java
+++ backend/src/main/java/com/skillshare/backend/model/Post.java
@@ -7,16 +7,25 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Represents a Post entity in the application.
+ * This class is mapped to the "posts" collection in MongoDB.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "posts")
 public class Post {
     @Id
-    private String id;
-    private String userId;
-    private String content;
-    private List<String> mediaUrls;
-    private List<String> mediaTypes; // corrected from String to List<String>
-    private Date createdAt = new Date();
+    private String id; // Unique identifier for the post
+
+    private String userId; // ID of the user who created the post
+
+    private String content; // Text content of the post
+
+    private List<String> mediaUrls; // List of URLs pointing to the media files associated with the post
+
+    private List<String> mediaTypes; // List of media types (e.g., "image", "video") corresponding to the media files
+
+    private Date createdAt = new Date(); // Timestamp indicating when the post was created
 }
